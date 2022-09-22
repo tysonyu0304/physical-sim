452f02d2fba9fcbe799850ca146845a601b26471
tysonyu0304 <88653545+tysonyu0304@users.noreply.github.com>
2022-09-22 19:12:23 +0800

    PEP8

--- L3_1.py
+++ L3_1.py
@@ -1,5 +1,4 @@
 from vpython import *
-import os
 
 height = 0
 v = 20
@@ -8,24 +7,26 @@ g = -9.8
 dt = 0.001
 max30 = 0
 max60 = 0
-t30 =0
-t60 =0
+t30 = 0
+t60 = 0
 
-sence = canvas(width = 600, height = 400, x = 0, y = 0, background = color.black)
-floor = box(pos = vec(5, 0, 0), length = 60, height = 0.01, width = 10, texture = textures.wood)
-ball30 = sphere(pos = vec(0, 0, 0), vc = vec(v * cos(30 * pi / 180), v * sin(30 * pi / 180), 0), radius = 0.5, color = color.blue, make_trail= True, trail_type = "curve", interval=1)
-ball60 = sphere(pos = vec(0, 0, 0), vc = vec(v * cos(60 * pi / 180), v * sin(60 * pi / 180), 0), radius = 0.5, color = color.green, make_trail= True, trail_type = "curve", interval=1)
+sence = canvas(width=600, height=400, x=0, y=0, background=color.black)
+floor = box(pos=vec(5, 0, 0), length=60, height=0.01, width=10, texture=textures.wood)
+ball30 = sphere(pos=vec(0, 0, 0), vc=vec(v*cos(30*pi/180), v*sin(30*pi/180), 0), radius=0.5,
+                color=color.blue, make_trail=True, trail_type="curve", interval=1)
+ball60 = sphere(pos=vec(0, 0, 0), vc=vec(v*cos(60*pi/180), v*sin(60*pi/180), 0), radius=0.5,
+                color=color.green, make_trail=True, trail_type="curve", interval=1)
 
 while ball30.pos.y >= 0 or ball60.pos.y >= 0:
-    rate(1 / dt)
+    rate(1/dt)
     if ball30.pos.y >= 0:
-        ball30.pos += ball30.vc * dt
+        ball30.pos += ball30.vc*dt
         t30 += dt
     if ball60.pos.y >= 0:
-        ball60.pos += ball60.vc * dt
+        ball60.pos += ball60.vc*dt
         t60 += dt
-    ball30.vc.y += g * dt
-    ball60.vc.y += g * dt
+    ball30.vc.y += g*dt
+    ball60.vc.y += g*dt
     if max30 < ball30.pos.y:
         max30 = ball30.pos.y
     if max60 < ball60.pos.y:
@@ -38,5 +39,3 @@ print("30度的水平射程", ball30.pos.x)
 print("60度的飛行時間", t60)
 print("60度的最大高度", max60)
 print("60度的水平射程", ball60.pos.x)
-input()
-os._exit(0) 
--- L3_3.py
+++ L3_3.py
@@ -1,9 +1,12 @@
 from vpython import *
-import os
 
-scene = canvas(width = 600 , height = 600 , center=vector(10, 0, 0), background=vector(0.605, 0.789, 0.789))
+scene = canvas(width=600 , height=600 , center=vec(10, 0, 0),
+               background=vector(0.605, 0.789, 0.789))
+scene.camera.pos = vec(-9, 0 ,0)
+scene.range = 20
 
-ball = sphere(radius = 1, color = color.yellow, make_trail = True, trail_type = "curve", interval = 1)
+ball = sphere(radius=1, color=color.yellow, make_trail=True,
+              trail_type="curve", interval=1)
 
 dt = 0.001
 t = 0
@@ -12,24 +15,20 @@ F = vec(4, 0, 0)
 ball.v = vec(0, 6.0, 0)
 ball.a = vec(0, 0, 0)
 
-def stop():
-    os.system("pause")
-
-while True:                     #無窮迴圈
+while True:        
     rate(1/dt)
     t += dt
     
     ball.a = F/m
-    ball.v = ball.v + ball.a*dt
-    ball.pos = ball.pos + ball.v*dt
+    ball.v += ball.a*dt
+    ball.pos += ball.v*dt
 
     F_vec = ball.v.rotate(angle = pi/2)  
-    # print(F_vec)  #請使用rotate的語法，找出F向量的方向
-    F = 4.0*F_vec/mag(F_vec)    #沿力的方向(F_vec)長出大小為4.0的力
+    F = 4.0*F_vec/mag(F_vec)
     
-    if ball.v.x >= 0 and ball.v.x + F.x/m < 0:    #右端點條件
+    if ball.v.x >= 0 and ball.v.x + F.x/m < 0:    
         right = ball.pos.x
-    if ball.v.x <= 0 and ball.v.x + F.x/m > 0:    #左端點條件
+    if ball.v.x <= 0 and ball.v.x + F.x/m > 0:    
         left = ball.pos.x
         print ("theoretical radius = ", m*mag(ball.v)**2/mag(F))
-        print ("simulated radius = " , abs(right - left)/2)    #模擬的半徑值
+        print ("simulated radius = " , abs(right - left)/2)    
