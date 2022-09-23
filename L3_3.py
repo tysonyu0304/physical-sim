3ac73fcde05a49145e16f080240ce6d3afa95175
tysonyu0304 <88653545+tysonyu0304@users.noreply.github.com>
2022-09-23 09:13:48 +0800

    edit

--- L3_3.py
+++ L3_3.py
@@ -2,7 +2,7 @@ from vpython import *
 
 scene = canvas(width=600 , height=600 , center=vec(10, 0, 0),
                background=vector(0.605, 0.789, 0.789))
-scene.camera.pos = vec(-9, 0 ,0)
+scene.camera.pos = vec(9, 0 ,0)
 scene.range = 20
 
 ball = sphere(radius=1, color=color.yellow, make_trail=True,
@@ -23,7 +23,7 @@ while True:
     ball.v += ball.a*dt
     ball.pos += ball.v*dt
 
-    F_vec = ball.v.rotate(angle = pi/2)  
+    F_vec = ball.v.rotate(angle = 3*pi/2)  
     F = 4.0*F_vec/mag(F_vec)
     
     if ball.v.x >= 0 and ball.v.x + F.x/m < 0:    
