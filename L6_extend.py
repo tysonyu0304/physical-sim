869fe99a5a4c31dabbb7324caf409a5532b7ddf2
tysonyu0304 <taiyou940304@gmail.com>
2022-10-26 18:40:35 +0800

    final?

--- L6_extend.py
+++ L6_extend.py
@@ -6,53 +6,64 @@ dt = 0.01 #時間間隔 0.01 秒
 run = False
 reset = False
 end = False
+mtrail = True
+speed = 2.0
 
 class ball():
     def __init__(self):
         self.size = 0.1
-        self.m1 = 2
-        self.m2 = 2
+        self.m = 2
         self.side = 4.0
         self.thk = 0.3
         self.s2 = 2*self.side - self.thk
         self.s3 = 2*self.side + self.thk
         self.num = 5
         self.ball = []
-        self.hit_t = 0
+        self.colors = [color.yellow, color.green, color.black, color.orange]
         self.start()
     
     def start(self):
         self.scene = canvas(width=800, height=800, center = vec(0,0,0), background=vec(0.6,0.8,0.8)) #設定畫面
-        #畫牆壁
-        self.wall_R = box (pos=vector(self.side, 0, 0), size=vector(self.thk, self.s2, self.s3),  color = color.red)
-        self.wall_L = box (pos=vector(-self.side, 0, 0), size=vector(self.thk, self.s2, self.s3),  color = color.red)
-        self.wall_B = box (pos=vector(0, -self.side, 0), size=vector(self.s3, self.thk, self.s3),  color = color.blue)
-        self.wall_T = box (pos=vector(0, self.side, 0), size=vector(self.s3, self.thk, self.s3),  color = color.blue)
-        self.wall_BK = box(pos=vector(0, 0, -self.side), size=vector(self.s2, self.s2, self.thk), color = color.gray(0.7))
-        self.num = int(input("請輸入球數:"))
-        self.ball_ganerater()
-        self.b1 = button(text="stop", pos=self.scene.caption_anchor, bind=self.run_stop)
-        self.b2 = button(text="reset", pos=self.scene.caption_anchor, bind=self.reset)
-        self.b3 = button(text="end", pos=self.scene.caption_anchor, bind=self.end)
-        global run
-        run = True
+        self.b_slide = slider(pos=self.scene.title_anchor, min=0, max=500, step=1, value=0, length=600, right=15, bind=self.b_slider)
+        self.b_text = wtext(pos=self.scene.title_anchor, text="ball numbers: 0")
+        self.b_n_apply = button(text="apply", pos=self.scene.title_anchor, bind=self.apply)
+        self.v_slide = slider(pos=self.scene.title_anchor, min=0.1, max=5.0, step=0.1, value=2.0, length=400, right=15, bind=self.v_slider)
+        self.v_apply = button(pos=self.scene.title_anchor, text="apply", bind=self.apply_v)
+        self.v_text = wtext(pos=self.scene.title_anchor, text="ball speed: 2.0")
+        self.wall_R = box(pos=vector(self.side, 0, 0), size=vector(self.thk, self.s2, self.s3),  color=color.white)
+        self.wall_L = box(pos=vector(-self.side, 0, 0), size=vector(self.thk, self.s2, self.s3),  color=color.white)
+        self.wall_B = box(pos=vector(0, -self.side, 0), size=vector(self.s3, self.thk, self.s3),  color=color.white)
+        self.wall_T = box(pos=vector(0, self.side, 0), size=vector(self.s3, self.thk, self.s3),  color=color.white)
+        self.wall_BK = box(pos=vector(0, 0, -self.side), size=vector(self.s2, self.s2, self.thk), color=color.gray(0.7))
+        self.b_run = button(text="run", pos=self.scene.caption_anchor, bind=self.run_stop)
+        self.b_reset = button(text="reset", pos=self.scene.caption_anchor, bind=self.reset)
+        self.b_end = button(text="end", pos=self.scene.caption_anchor, bind=self.end)
+        self.trail = wtext(pos=self.scene.caption_anchor, text="Make Trail")
+        self.b_mtrail = button(text="No!", pos=self.scene.caption_anchor, bind=self.mtrail)
 
     def init(self):
         global reset
         self.num = 0
         self.ball = []
         self.scene.visible = False
-        self.b1.delete()
-        self.b2.delete()
-        self.b3.delete()
+        self.b_slide.delete()
+        self.b_n_apply.delete()
+        self.b_text.text = ""
+        self.trail.text = ""
+        self.b_text.delete()
+        self.trail.delete()
+        self.b_mtrail.delete()
+        self.b_run.delete()
+        self.b_reset.delete()
+        self.b_end.delete()
         self.scene.delete()
         self.start()
         reset = False
 
     def pos_ganerater(self):
-        x = random.uniform(-((self.s2/2 - self.thk)), ((self.s2/2 - self.thk)))
-        y = random.uniform(-((self.s2/2 - self.thk)), ((self.s2/2 - self.thk)))
-        z = random.uniform(-((self.s2/2 - self.thk)), ((self.s2/2 - self.thk)))
+        x = random.uniform(-((self.s2/2 - self.thk - self.size)), ((self.s2/2 - self.thk - self.size)))
+        y = random.uniform(-((self.s2/2 - self.thk - self.size)), ((self.s2/2 - self.thk - self.size)))
+        z = random.uniform(-((self.s2/2 - self.thk - self.size)), ((self.s2/2 - self.thk - self.size)))
         return vec(x, y, z)
 
     def v_ganerater(self):
@@ -60,25 +71,24 @@ class ball():
         y = random.uniform(-2, 2)
         z = random.uniform(-2, 2)
         v = vec(x, y, z)
-        return v/mag(v)*2
+        return v/mag(v)
 
     def ball_ganerater(self):
         for i in range(self.num):
-            self.ball.append(sphere(pos=self.pos_ganerater(), v=self.v_ganerater(), radius=self.size, color=color.yellow, make_trail=True, retain=100)) #畫好多球
+            self.ball.append(sphere(pos=self.pos_ganerater(), v=self.v_ganerater()*speed, radius=self.size, color=self.colors[random.randint(0,3)], make_trail=mtrail, retain=100)) #畫好多球
 
     def collision(self, v1, v2, x1, x2):
-        v1_f = v1 - (2*self.m2/(self.m1 + self.m2))*((dot(v1 - v2, x1 - x2))/(mag(x1 - x2)**2))*(x1 - x2)   #請嘗試將碰撞公式填入其中
-        v2_f = v2 - (2*self.m1/(self.m1 + self.m2))*((dot(v2 - v1, x2 - x1))/(mag(x2 - x1)**2))*(x2 - x1)
+        v1_f = v1 - (2*self.m/(self.m + self.m))*((dot(v1 - v2, x1 - x2))/(mag(x1 - x2)**2))*(x1 - x2)   #請嘗試將碰撞公式填入其中
+        v2_f = v2 - (2*self.m/(self.m + self.m))*((dot(v2 - v1, x2 - x1))/(mag(x2 - x1)**2))*(x2 - x1)
         return (v1_f, v2_f)
 
     def btb_collision(self):
         for i in range(len(self.ball)):
-            for j in range(i+1, len(self.ball)):
+            for j in range(i+1, len(self.ball)-1):
                 #若兩球球心距離小於兩倍球半徑，則代入碰撞公式改變速度
-                if mag(self.ball[i].pos - self.ball[i+1].pos) < 2*self.size :
-                    self.ball[i].v, self.ball[i+1].v = self.collision(self.ball[i].v, self.ball[i+1].v, self.ball[i].pos, self.ball[i+1].pos)
-                    self.hit_t += 1
-                    print(f"hit! {self.hit_t} times")
+                if mag(self.ball[i].pos - self.ball[j].pos) < 2*self.size :
+                    self.ball[i].v, self.ball[j].v = self.collision(self.ball[i].v, self.ball[j].v, self.ball[i].pos, self.ball[j].pos)
+                    print("Hit!")
                 
     def btw_collision(self):
         for i in self.ball:
@@ -93,12 +103,12 @@ class ball():
         for i in self.ball:
             i.pos = i.pos + i.v*dt
 
-    def run_stop(self, b1):
+    def run_stop(self, b_run):
         global run
         if run == False:
-            b1.text = "stop"
+            b_run.text = "stop"
         elif run == True:
-            b1.text = "run"
+            b_run.text = "run"
         run = not run
     
     def reset(self):
@@ -109,9 +119,43 @@ class ball():
         global end
         end = True
 
-    def changeSize(self):
+    def b_slider(self):
+        self.b_text.text = f"ball numbers: {self.b_slide.value}"
+
+    def apply(self):
         for i in self.ball:
-            i.radius = self.size
+            i.clear_trail()
+            i.visible = False
+        self.ball = []
+        self.num = self.b_slide.value
+        self.ball_ganerater()
+        self.b_run.text = "stop"
+        global run
+        run = True
+    
+    def mtrail(self):
+        global mtrail
+        if mtrail == True:
+            mtrail = False
+            for i in self.ball:
+                i.make_trail = False
+                i.clear_trail()
+            self.b_mtrail.text = "Yes!"
+        elif mtrail == False:
+            mtrail = True
+            for i in self.ball:
+                i.make_trail = True
+            self.b_mtrail.text = "No!"
+
+    def v_slider(self):
+        self.v_text.text = f"ball speed: {self.v_slide.value}"
+
+    def apply_v(self):
+        global speed
+        for i in self.ball:
+            i.v = i.v/speed*self.v_slide.value
+        speed = self.v_slide.value
+        
 
 Ball = ball()
 
